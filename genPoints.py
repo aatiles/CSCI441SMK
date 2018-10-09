b3915ceac07a31512401f14d49d239773270ce6b
William Brickner <wbrickner@mines.edu>
2018-10-09 15:31:08 -0600

    Updated csv format

--- genPoints.py
+++ genPoints.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         raise TypeError("No command line arguments")
     else:
-        print(int(sys.argv[1])*int(sys.argv[2]))
+        print(sys.argv[1], sys.argv[2])
         for i in range(int(sys.argv[1])):
             for j in range(int(sys.argv[2])):
-                print(i,0,j)
+                print(i,0,j, end=" ")
+            print()
 
