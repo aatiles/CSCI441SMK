85fa9bd5f5b055c734d175324d90945e9881c305
William Brickner <wbrickner@bb136-11.mines.edu>
2018-10-10 18:36:52 -0600

    Added a singular curved patch

--- src/genPoints.py
+++ src/genPoints.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
 import sys
-
+import random
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         raise TypeError("No command line arguments")
     else:
-        print(sys.argv[1], sys.argv[2])
-        for i in range(int(sys.argv[1])):
-            for j in range(int(sys.argv[2])):
-                print(i,0,j, end=" ")
-            print()
-
+        width = int(sys.argv[1])*3 + 1
+        height = int(sys.argv[2])*3 + 1
+        print(width, height)
+        grid = [[[i,random.uniform(0,3),j] for j in range(width)] for i in range(int(height))]
+        for row in grid:
+            for point in row:
+                print(" ".join(map(str, point)),end=" ") 
+            print() 
+        
