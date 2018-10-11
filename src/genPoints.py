c8a92b7925c8e487491da6500299cb8bc79c7f98
William Brickner <wbrickner@bb136-11.mines.edu>
2018-10-10 19:24:39 -0600

    Have a method for generating almost C1 continuous surfaces.

--- src/genPoints.py
+++ src/genPoints.py
@@ -3,13 +3,25 @@
 import sys
 import random
 if __name__ == "__main__":
+    random.seed(123)
     if len(sys.argv) == 1:
         raise TypeError("No command line arguments")
     else:
         width = int(sys.argv[1])*3 + 1
         height = int(sys.argv[2])*3 + 1
         print(width, height)
-        grid = [[[i,random.uniform(0,3),j] for j in range(width)] for i in range(int(height))]
+        grid = [[[i,round(random.uniform(0,3),2),j] for j in range(width)] for i in range(int(height))]
+        for i in range(3,height-1,3):
+            for j in range(width):
+                grid[i][j][1] = round((grid[i+1][j][1] + grid[i-1][j][1])/2,2)
+        for j in range(3,width-1,3):
+            for i in range(height):
+                grid[i][j][1] = round((grid[i][j+1][1] + grid[i][j-1][1])/2,2)
+
+                            
+
+                
+        
         for row in grid:
             for point in row:
                 print(" ".join(map(str, point)),end=" ") 
